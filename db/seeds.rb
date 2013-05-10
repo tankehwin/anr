1b31707c4e547c4feec43ea5b7041defe0fb5a1e
Zaid M. Said <sentul@brainstorm.pro>
2013-05-10 11:52:45 +0800

    removed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,32 +6,32 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-puts "Seeding.."
+# puts "Seeding.."
 
-# config
-number_of_player = 9
-number_of_tournament = 2
+# # config
+# number_of_player = 9
+# number_of_tournament = 2
 
-Player.delete_all
-Player.create({:id => 1, :username => "MY00000", :email => "admin@sentulasia.com", :name => "Bye"}, :as => :seed)
-puts "Seeding #{number_of_player} players..."
-(1..number_of_player).each do |p|
-  Player.create :username => "MY0000" + p.to_s,
-  :email => "player#{p}@sentulasia.com",
-  :name => "Player #{p}"
-  print "#{p}..."
-  STDOUT.flush
-end
-puts "Done!"
+# Player.delete_all
+# Player.create({:id => 1, :username => "MY00000", :email => "admin@sentulasia.com", :name => "Bye"}, :as => :seed)
+# puts "Seeding #{number_of_player} players..."
+# (1..number_of_player).each do |p|
+#   Player.create :username => "MY0000" + p.to_s,
+#   :email => "player#{p}@sentulasia.com",
+#   :name => "Player #{p}"
+#   print "#{p}..."
+#   STDOUT.flush
+# end
+# puts "Done!"
 
-Tournament.delete_all
-Participant.delete_all
-puts "Seeding #{number_of_tournament} tournaments..."
-(1..number_of_tournament).each do |t|
-  tournament = Tournament.create :name => "Tournament " + t.to_s
-  print "#{t}..."
-  STDOUT.flush
-end
-puts "Done!"
+# Tournament.delete_all
+# Participant.delete_all
+# puts "Seeding #{number_of_tournament} tournaments..."
+# (1..number_of_tournament).each do |t|
+#   tournament = Tournament.create :name => "Tournament " + t.to_s
+#   print "#{t}..."
+#   STDOUT.flush
+# end
+# puts "Done!"
 
-puts "Seeding Done!"
+# puts "Seeding Done!"
